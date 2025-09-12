2dd5eae4c8a92fa2331f3f5739702c62d057a3fa
SandrinePichla <sandrinepichla@hotmail.com>
2025-09-12 02:08:09 +0200

    Modif contact

--- server/routes/contact.routes.js
+++ server/routes/contact.routes.js
@@ -1,5 +1,5 @@
- const express = require('express');
-import nodemailer from 'nodemailer';
+const express = require('express');
+const nodemailer = require('nodemailer');
 
 const router = express.Router();
 
